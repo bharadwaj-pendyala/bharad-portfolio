7d5326ef1ad1627daee8f6f133726f72e76076ba
Bharadwaj <bharadwajpendyala@gmail.com>
2025-08-09 23:56:33 -0500

    fix: reduce spacing to more reasonable levels for better visual balance

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -32,7 +32,7 @@ export default function Blog() {
       </div>
 
       {/* Blog Posts */}
-      <section className="space-y-16">
+      <section className="space-y-12">
         {/* Blog Post 1 */}
         <article className="group">
           <Link href="/blog/post-1" className="block">
